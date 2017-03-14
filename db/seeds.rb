847c4ee1cca64951ad8e0e54b84681f77e7f3bae
Caroline Olijve <olijvecaroline@gmail.com>
2017-03-14 19:45:49 +0100

    met tussentabel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
 User.destroy_all
 
-event = Event.create(name: "Zappa in A", description: "Zappa will be visiting the netherlands", location: "Melkweg", price: 100, capacity: 600, includes_food: true, includes_drinks: true, starts_at:(12.15), active: true)
-
 miria = User.create(email: "miriam@codaisseurbnb.com", password: "abcd1234")
+
+zappa = Category.create(name:"zappa")
+blues = Category.create(name:"reno fernis")
+barok = Category.create(name:"bach")
+romantiek = Category.create(name:"Haydn")
+amerikaans = Category.create(name:"Gershwin")
+
+event = Event.create(name: "Zappa in A", description: "Zappa will be visiting the netherlands", location: "Melkweg", price: 100, capacity: 600, includes_food: true, includes_drinks: true, starts_at:(12.15), active: true, user: miria, categories: [zappa, blues])
