ed13b4b954d48a0ef831d620474eca62feaa52f8
Caroline Olijve <olijvecaroline@gmail.com>
2017-03-15 11:29:26 +0100

    factory girl en faker installed

---
+++ spec/factories/events.rb
@@ -0,0 +1,23 @@
+FactoryGirl.define do
+  factory :event do
+    name                "House"
+    description         { Faker::Lorem.sentence(1) }
+    location            "Melkweg"
+    price               40
+    capacity            500
+    includes_food       true
+    includes_drinks     { Faker::Lorem.sentence(40) }
+    starts_at           { Faker::Address.city }
+    active              true
+    user                { build(:user) }
+    categories          { build (:category)}
+
+    trait :active do
+      active true
+    end
+
+    trait :inactive do
+      active false
+    end
+  end
+end
